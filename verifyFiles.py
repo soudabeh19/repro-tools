06455d6f68f732a090366ce38127c26bf9325bf8
Lalet Scaria <laletscaria@yahoo.co.in>
2016-11-09 22:39:48 +0100

    Updated functions for directory traversal and to get status info of each file and directory

--- verifyFiles.py
+++ verifyFiles.py
@@ -8,6 +8,7 @@
 import os
 import sys
 import subprocess
+from collections import OrderedDict
 
 total = len(sys.argv)
 cmdargs = str(sys.argv)
@@ -15,22 +16,32 @@ print ("The total numbers of args passed to the script: %d " % total)
 print ("Args list: %s " % cmdargs)
 print ("Script name: %s" % str(sys.argv[0]))
 print ("First argument: %s" % str(sys.argv[1]))
-file_dict = {}
-for root, dirs, files in os.walk(sys.argv[1]):
-    dirs[:] = (map(lambda d: os.path.join(root, d), dirs))
-    files[:] = (map(lambda f: os.path.join(root, f), files))
-    files.sort(key=lambda x: os.stat(os.path.join(root, x)).st_mtime)
-    #file_dict = {}
-    for dirname in dirs:
-        dirname = os.path.join(root,dirname)
-	
-	#print dirname
+fileNamesAndDirArray = []
+file_dir_dict = OrderedDict()
 
-    for filename in files:
-        filename = os.path.join(root, filename)
-        stat = os.stat(filename)
-	file_dict[filename] = stat
-        
-        #print filename
-print file_dict
+#Method list_files_and_dirs is used for listing files and directories present in the input directory.
+#Input Argument : Directory Path
+def list_files_and_dirs(dir):
+        listFileAndDirs = []
+        for root, dirs, files in os.walk(dir):
+           for dirname in dirs:
+              listFileAndDirs.append(os.path.join(root, dirname))
+           for name in files:
+              listFileAndDirs.append(os.path.join(root, name))
+        listFileAndDirs.sort(key=lambda x: os.path.getmtime(x))
+        return listFileAndDirs
 
+#Method populate_file_dir_dict is an ordered  python dictionary to save the status details of
+#each file and directory present in the listFileAndDirs list
+#Input parameter : List contianing the details of the path of each file and directory.
+def populate_file_dir_dict(listFileAndDirs):
+        temp_dict = OrderedDict()
+        for path in listFileAndDirs:
+           dirDetails = os.stat(path)
+           temp_dict[path]= dirDetails
+        return temp_dict
+
+fileNamesAndDirArray=list_files_and_dirs(sys.argv[1])
+file_dir_dict=populate_file_dir_dict(fileNamesAndDirArray)
+print fileNamesAndDirArray
+print file_dir_dict
