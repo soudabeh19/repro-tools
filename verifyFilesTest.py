5209c21785bbb14b49e248396a9c39eda02c5b2d
Lalet Scaria <laletscaria@yahoo.co.in>
2016-11-24 18:21:30 +0100

    Python unit test case file

---
+++ verifyFilesTest.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.case import TestCase
+from verifyFiles import *
+
+#VerifyFilesTest.py contains test cases that verify the correct functionality of the methods in the verifyFiles.py 
+#python script file
+
+class VerifyFilesTestCase(TestCase)
+   def testListFilesAndDirs(self):
+       assert True
+
+   def testPopulateFileDirDict(self):
+	assert True
+
+
+if __name__ = '__main__'
+   unittest.main()
