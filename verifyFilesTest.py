f168f3f6b6f26ebac7840a7da1b308810f1118fb
Lalet Scaria <laletscaria@yahoo.co.in>
2016-12-01 16:39:19 +0100

    Updated code for testListFilesAndDirs

--- verifyFilesTest.py
+++ verifyFilesTest.py
@@ -3,6 +3,8 @@ import verifyFiles
 import tempfile
 import sys
 import os
+import shutil
+import glob
 from io import StringIO
 
 #VerifyFilesTest.py contains test cases that verify the correct functionality of the methods in the verifyFiles.py 
@@ -30,8 +32,33 @@ class VerifyFilesTestCase(unittest.TestCase):
       self.assertEqual(directory_list,read_file_content)
 
 
-  #def testListFilesAndDirs(self):
-  #assert True
+   def testListFilesAndDirs(self):
+      listFileAndDirs=[]
+      tmpDir=tempfile.mkdtemp()
+      tmpListFileAndDirs=[]
+      tmpFileName1='myFile1'
+      tmpFileName2='myFile2'
+      fileDir1=os.path.join(tmpDir,tmpFileName1)
+      fileDir2=os.path.join(tmpDir,tmpFileName2)
+      try:
+         with open(fileDir1, "w") as tmp:
+            tmp.write("/home/S1C1\n/home/S2C2\n")
+         with open(fileDir2, "w") as tmp:
+            tmp.write("/home/S1C1\n/home/S2C2\n")
+         listFileAndDirs=verifyFiles.list_files_and_dirs(tmpDir)
+         files = glob.glob(tmpDir + "*")
+         files.sort(key=lambda x: os.path.getmtime(x))
+         #dirs.sort(key=lambda s: os.path.getmtime(os.path.join(tmpDir, s)))
+         print files
+      except IOError as e:
+         print 'IOError'
+      else:
+         os.remove(fileDir1)
+         os.remove(fileDir2)
+      finally:
+         shutil.rmtree(tmpDir)
+      print listFileAndDirs
+      self.assertEqual(listFileAndDirs,tmpListFileAndDirs)
 
   #def testPopulateFileDirDict(self):
   #assert True
