760e12fcc7fc853eb7257e2e12bd799b19a7c1f0
Tristan Glatard <tristan.glatard@concordia.ca>
2017-08-03 22:22:37 -0400

    Dice metric now supports Freesurfer's mgz files

--- metrics/dice.sh
+++ metrics/dice.sh
@@ -3,6 +3,19 @@
 set -e
 set -u
 
+function convert {
+    local image=$1
+    local ext=$(echo ${image} | awk -F '.' '{print $NF}')
+    if [ "${ext}" == "mgz" ]
+    then
+	local name=$(basename ${image} ${ext})
+	mri_convert ${image} ${name}.nii.gz &>/dev/null
+	echo ${name}.nii.gz
+    else
+	echo ${image}
+    fi
+}
+
 if [ $# != 2 ]
 then
     echo "Usage: $0 image1.nii.gz image2.nii.gz"
@@ -10,8 +23,8 @@ then
     exit 1
 fi
 
-im1=$1
-im2=$2
+im1=$(convert $1)
+im2=$(convert $2)
 mul=$(mktemp mul-XXXXX.nii.gz)
 fslmaths ${im1} -mul ${im2} ${mul}
 inter=$(fslstats ${mul} -V | awk '{print $1}')
