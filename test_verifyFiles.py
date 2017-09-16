40e6042581e5abe57940111b955759770cf54b53
Lalet Scaria <laletscaria@yahoo.co.in>
2017-09-15 20:31:24 -0400

    Updated test file

--- test_verifyFiles.py
+++ test_verifyFiles.py
@@ -4,6 +4,7 @@ from verifyFiles import get_dir_dict
 from verifyFiles import checksum 
 from verifyFiles import read_file_contents
 from verifyFiles import get_conditions_dict
+from verifyFiles import get_conditions_checksum_dict
 
 def test_checksum():
   assert checksum("test/condition4") == "45a021d9910102aac726dd222a898334"
@@ -12,8 +13,13 @@ def test_dir_dict(tmpdir):
   assert get_dir_dict("test/condition4","test/exclude_items.txt")
 
 def test_conditions_dict():
+  conditions_dict = get_mock_conditions_dict()
+  assert conditions_dict['condition4'].keys()==conditions_dict['condition5'].keys()
+
+def get_mock_conditions_dict():
   conditions_list=read_file_contents("test/conditions.txt")
-  conditions_dict = get_conditions_dict(conditions_list,"test","test/exclude_items.txt")
-  assert conditions_dict['condition4'].keys()==conditions_dict['condition5'].keys() 
+  return get_conditions_dict(conditions_list,"test","test/exclude_items.txt") 
 
-
+def test_conditions_checksum_dict():
+  conditions_dict = get_mock_conditions_dict()
+  assert get_conditions_checksum_dict(conditions_dict,"test","checksums-after.txt")  
