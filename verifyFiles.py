5adab3e7610ad39ce6815ab6df569a7ee371513c
soudabeh <s_arghi@encs.concordia.ca>
2017-10-11 10:57:51 -0400

    removed args.fileDiff from the log_info

--- verifyFiles.py
+++ verifyFiles.py
@@ -512,7 +512,7 @@ def main():
                                              /home/$(USER)/CentOS7.FSL5.0.6
                                              Each directory will contain subject folders like 100307,100308 etc'''))
         parser.add_argument("-c", "--checksumFile",help="Reads checksum from files. Doesn't compute checksums locally")
-	parser.add_argument("difference", help="Writes the difference matrices and indexs into files")
+	parser.add_argument("difference", help="Base name to use in output file names")
         parser.add_argument("-m", "--metricsFile", help="CSV file containing metrics definition. Every line contains 4 elements: metric_name,file_extension,command_to_run,output_file_name") 
         parser.add_argument("-e","--excludeItems",help="The list of items to be ignored while parsing the files and directories")
 	parser.add_argument("-k","--checkCorruption",help="If this flag is kept 'TRUE', it checks whether the file is corrupted")
@@ -554,7 +554,7 @@ def main():
         #diff,metric_values,dictionary_executables,dictionary_processes=n_differences_across_subjects(conditions_dict,root_dir,metrics,checksums_from_file_dict,args.checksumFile,args.checkCorruption,args.sqLiteFile)i
 	diff,bDiff,metric_values,dictionary_executables,dictionary_processes,metric_values_subject_wise=n_differences_across_subjects(conditions_dict,root_dir,metrics,checksums_from_file_dict,args.checksumFile,args.checkCorruption,args.sqLiteFile,args.trackProcesses)
        	if args.difference is not None:
-            log_info("Writes the difference matrices and indexs into files"+args.difference)
+            log_info("Writes the difference matrices and indexs into files")
             diff_file = open(args.difference+"_differences_subject_total.txt",'w')
             diff_file.write(pretty_string(diff,conditions_dict))
 	    write_text_files (bDiff,conditions_dict,args.difference)
