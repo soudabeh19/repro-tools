a4876540447ad25e397cff8451e0e3f3704c1bdb
soudabeh <s_arghi@encs.concordia.ca>
2017-10-24 12:25:55 -0400

    Used findspark for the ImportError

--- test_predict.py
+++ test_predict.py
@@ -4,6 +4,9 @@ from argparse import ArgumentParser
 import os, shutil, sys
 if sys.version >= '3':
     long = int
+import findspark
+    findspark.init()
+
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, Row
 from pyspark.ml.evaluation import RegressionEvaluator
