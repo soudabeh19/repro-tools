814898433ef11ee90a60891e3dda233eea9c29c8
Soudabeh Barghi <binrepro@gra-login2.graham.sharcnet>
2018-03-16 07:35:45 -0400

    Changed the input files to smaller sample

--- test/test_plot_superimpose_matrix.py
+++ test/test_plot_superimpose_matrix.py
@@ -2,7 +2,7 @@ import pytest
 import commands
 
 def test_run():
-    command_line_string = "python plot_superimpose_matrix.py test/test_differences_plot.txt test/test_predict_plot.txt test/test_superimpose_plot.pdf"
+    command_line_string = "python plot_superimpose_matrix.py test/predict_test/test_differences.txt test/predict_test/triangular-S_0.6_test_data_matrix.txt test/test_superimpose_plot.png"
     return_value, output = commands.getstatusoutput(command_line_string)
     print (return_value)
     assert not return_value, output
