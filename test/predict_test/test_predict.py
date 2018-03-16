1ba6a3c8079560b5bfb477d1530e1b751555ca84
Soudabeh Barghi <binrepro@gra-login3.graham.sharcnet>
2018-03-15 21:18:45 -0400

    Chenged the input file's name

--- test/predict_test/test_predict.py
+++ test/predict_test/test_predict.py
@@ -5,6 +5,6 @@ import sys
 
 #shuffled_subject = [1,4,2,0,3]
 def test_run():
-      command_line_string = "python predict.py test/predict_test/test_diff.txt 0.6 triangular-S -s 3"
+      command_line_string = "python predict.py test/predict_test/test_differences.txt 0.6 triangular-S -s 3"
       return_value, output = commands.getstatusoutput(command_line_string)
       assert not return_value, output
