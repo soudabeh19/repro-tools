3ea0d1a54390ed7cea38035f32de3855ff1dcd14
Soudabeh Barghi <binrepro@gra-login3.graham.sharcnet>
2018-03-26 13:36:36 -0400

    Updated after merging plot_matrix.py and plot_superimpose_matrix.py

--- test/test_plot_superimpose_matrix.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import commands
-
-def test_run():
-    command_line_string = "python plot_superimpose_matrix.py test/predict_test/test_differences.txt test/predict_test/triangular-S_0.6_test_data_matrix.txt test/test_superimpose_plot.png"
-    return_value, output = commands.getstatusoutput(command_line_string)
-    print (return_value)
-    assert not return_value, output
-
