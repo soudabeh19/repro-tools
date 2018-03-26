d55dc61734597ac2b03f42c9e32a027b3d56a035
Soudabeh Barghi <binrepro@gra-login3.graham.sharcnet>
2018-03-26 11:44:34 -0400

    Merged plot_matix.py with plot_superimpose_matrix.py

--- plot_superimpose_matrix.py
+++ plot_superimpose_matrix.py
@@ -34,13 +34,11 @@ def main():
 
     parser.add_argument("original_matrix", help = "Matrix file to plot. Each row should be in the following format: <ignored> <subject_id> <binary_difference> <file_id>. File ids should be ordered according to their latest modification time.")# plotting original-matrix (row_index, subject, binary_difference, ordered_file_id)
 
-    parser.add_argument("test_matrix", help = "Matrix file to plot. Each row should be in the following format: <file_id> <subject_id> <ignored> <predic_value>. File ids should be ordered according to their latest modification time.")# plotting test (ordered_file_id, subject, binary_difference, predict_value)
+    parser.add_argument("--test_matrix","-t", help = "Matrix file to plot. Each row should be in the following format: <file_id> <subject_id> <ignored> <predic_value>. File ids should be ordered according to their latest modification time.")# plotting test (ordered_file_id, subject, binary_difference, predict_value)
 
     parser.add_argument("output_file", help = "Output file where the plot will be saved. File type is determined from extension.")
     args=parser.parse_args()
-
     original_matrix = parse_matrix(args.original_matrix, True)
-    test_matrix = parse_matrix(args.test_matrix, False)
     assert(original_matrix.get(0))
     n = np.zeros(shape=(len(original_matrix), len(original_matrix[0])))
     for file_id in original_matrix.keys():
@@ -48,18 +46,20 @@ def main():
         for subject_id in subject_dict.keys():
             original_value = subject_dict[subject_id]
             n[file_id, subject_id] = original_value
-            if test_matrix.get(file_id) == None or test_matrix[file_id].get(subject_id) == None: # file and subject are in the test set
-                continue
-            if test_matrix[file_id][subject_id] == original_value: # no difference
-                if test_matrix[file_id][subject_id] == 1:
-                    n[file_id, subject_id] = 2
-                else:
-                    n[file_id, subject_id] = 3
-            else: # there is a difference
-                if test_matrix[file_id][subject_id] == 1:
-                    n[file_id, subject_id] = 5
-                else:
-                    n[file_id, subject_id] = 4
+            if args.test_matrix is not None:
+                test_matrix = parse_matrix(args.test_matrix, False)
+                if test_matrix.get(file_id) == None or test_matrix[file_id].get(subject_id) == None: # file and subject are in the test set
+                        continue
+                if test_matrix[file_id][subject_id] == original_value: # no difference
+                    if test_matrix[file_id][subject_id] == 1:
+                        n[file_id, subject_id] = 2
+                    else:
+                        n[file_id, subject_id] = 3
+                else: # there is a difference
+                    if test_matrix[file_id][subject_id] == 1:
+                        n[file_id, subject_id] = 5
+                    else:
+                        n[file_id, subject_id] = 4
     cmap = matplotlib.colors.ListedColormap(['#000000', '#FFFFFF', '#DDFFDD', '#777777', '#FFFF00', '#FF0000'])
     plt.matshow(n, interpolation = 'nearest', aspect='auto', cmap=cmap)
     plt.colorbar()
